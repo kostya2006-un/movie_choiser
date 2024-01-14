48ef609abe4efaf1c8dcc0dbed9e9c5eae36dda7
Kostya <run06mail.ru@gmail.com>
2024-01-14 20:38:13 +0300

    Start my project. Second commit. Created first settings for django. And settings for database. Also some alogoritms with orm to add data in data base

--- movie/app/views.py
+++ movie/app/views.py
@@ -1,10 +1,3 @@
 from django.shortcuts import render
-from app.top500 import kino
 from django.views import View
 
-
-class IndexView(View):
-    template_name = 'app/index.html'
-
-    def get(self,request):
-        return render(request,self.template_name)
